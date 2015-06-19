7b49137bfb59e6f93da748baf537d8444db6e0f0
Andrea Baker <ambaker.888@gmail.com>
2015-06-19 19:34:13 -0400

    added jquery to hide modals when clicked

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function () {
   jQuery(document).on('click','[data-show]', function (e) {
     e.preventDefault();
+    jQuery('.modal-content').each(function(){
+      jQuery(this).hide();
+    });
     var target = jQuery(this).data("show");
     console.log(target);
-    jQuery(this).closest('.modal-content').hide();
     jQuery(target).show();
   });
 });
